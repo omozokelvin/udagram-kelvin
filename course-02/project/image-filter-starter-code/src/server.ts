08001778675154262ab0b1a9bc6071746b7f080e
Kelvin Omozokpia <kelvinjune2@gmail.com>
2022-06-27 02:15:29 +0100

    Completed project deployment

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +29,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get('/', async (req, res) => {
+    res.status(200).send('try GET /filteredimage?image_url={{}}');
+  });
+
+  app.get('/filteredImage', async (req, res) => {
+    try {
+      const { image_url } = req.query;
+      if (!image_url) {
+        return res.status(400).send('Image url is required');
+      }
+
+      const filteredpath = await filterImageFromURL(image_url);
+
+      res.sendFile(filteredpath, async () => {
+        await deleteLocalFiles([filteredpath]);
+      });
+    } catch (error) {
+      res.status(422).send('Failed to process this image');
+    }
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
