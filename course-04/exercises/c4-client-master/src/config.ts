1e3807fdfdda5ec17e07825306bac40c5d99eae4
Kelvin Omozokpia <kelvinjune2@gmail.com>
2022-08-20 02:01:57 +0100

    Updated config

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'nhmk625tw0'
+const apiId = 'a8m30het64'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'test-endpoint.auth0.com',
-  clientId: 'A72UMiQQJzmHQRPpe1dHOyq3EfPvo3Hy',
+  domain: 'darkel.us.auth0.com',
+  clientId: 'tTbQ1k4WJK4KxM5eJnJ9Pt0DAOCDEwFh',
   callbackUrl: 'http://localhost:3000/callback'
 }
